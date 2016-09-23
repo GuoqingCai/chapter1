e040a49524b97ace7c7530893fdf74a4a4d78051
Tufo <cgq0824@126.com>
2016-09-23 17:01:07 +0800

    修改IDEA界面配置

--- src/main/java/org/smart4j/chapter1/HelloServlet.java
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -14,13 +14,13 @@ import java.util.Date;
  */
 
 @WebServlet("/hello")
-public class HelloServlet extends HttpServlet{
+public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String currentTime=dataFormat.format(new Date());
+        String currentTime = dataFormat.format(new Date());
         //System.out.println(currentTime);
-        req.setAttribute("currentTime",currentTime);
-        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
     }
 }
